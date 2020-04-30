7e50e521cfc3d87691df38d8e99e4f2fd34c7477
Jonathan Phillips <jonnyphillips1995@gmail.com>
2020-04-30 09:34:43 -0500

    Lesson 5 Exercise

---
+++ src/com/fundamentals/java/Lesson5Exercise.java
@@ -0,0 +1,23 @@
+package com.fundamentals.java;
+
+public class Lesson5Exercise {
+
+    //the homework number is 29
+    int myDecimal = 29;
+    double myHexadecimal = 1D;
+    int myBinary = 11101;
+
+    int myInt2 = 2_000;
+    double twiceIsRight = 1.0e+5D;
+    short myShort2 = 1_000;
+    long myLong2 = 1_500;
+    byte myByte2 = 65;
+    float myFloat2 = 3.0e-3F;
+
+    public void wideConversions(){
+
+        int byteInt = myByte2;
+        long shortLong = myShort2;
+        float intFloat = myInt2;
+    }
+}
--- src/com/fundamentals/java/Main.java
+++ src/com/fundamentals/java/Main.java
@@ -5,18 +5,36 @@ package com.fundamentals.java;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello Java");
-        System.out.println("This is my first sentence");
-        System.out.println("I am new to Java");
-        System.out.println("This Java course is cool");
-        System.out.println("I am learning stuff everyday");
-
-        myHouseExample();
+        lesson5Exercises();
+        //lesson5Examples();
+        // write your code here
+        //System.out.println("Hello Java");
+        //System.out.println("This is my first sentence");
+        //System.out.println("I am new to Java");
+        //System.out.println("This Java course is cool");
+        //System.out.println("I am learning stuff everyday");
     }// End main method
 
-    public static void myHouseExample(){
-        House myHouse = new House();
-        myHouse.doorOpenClose();
-    }// End myHouseExample
-}// end class Main
+    public static void lesson5Examples () {
+        Lesson5 myFive = new Lesson5();
+        myFive.showLocalExample();
+        int example = myFive.showLocalWithReturn(5,7);// return to a variable
+        System.out.println(example); //print variable from the line above
+        System.out.println(myFive.showLocalWithReturn(2,9)); //direct return of variable
+        myFive.showInstanceVariable();
+        myFive.showConversions();
+    }// end lesson 5 Examples
+
+    public static void lesson5Exercises(){
+        Lesson5Exercise myHomework = new Lesson5Exercise();
+        myHomework.wideConversions();
+        System.out.println("myHomework.wideConversions");
+
+    }
+
+
+        public void myHouseExample(){
+            House myHouse = new House();
+            myHouse.doorOpenClose();
+        }// End myHouseExample
+}
